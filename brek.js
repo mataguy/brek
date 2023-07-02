82c96c14c5283c452f092ec69c3ab2be3d66c03c
mataguy <138390606+mataguy@users.noreply.github.com>
2023-07-02 20:52:35 +0400

    Add files via upload

---
+++ brek.js
@@ -0,0 +1,23 @@
+function sloj(){
+    let a = +document.form1.an.value;
+    let b = +document.form1.bn.value;
+    alert(a + b);
+}
+
+function umnoj(){
+    let a = +document.form1.an.value;
+    let b = +document.form1.bn.value;
+    alert(a * b);
+}
+
+function del(){
+    let a = +document.form1.an.value;
+    let b = +document.form1.bn.value;
+    alert(a / b);
+}
+
+function vichit(){
+    let a = +document.form1.an.value;
+    let b = +document.form1.bn.value;
+    alert(a - b);
+}
